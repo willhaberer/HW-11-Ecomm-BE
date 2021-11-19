cc0b1b29b188c9462a8e16edf01455ea7cdbdea6
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-18 20:58:46 -0500

    created delete route for categories by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,8 +44,24 @@ router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const catData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!catData) {
+      res.status(404).json({ message: "No Category found with this id!" });
+      return;
+    }
+
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
