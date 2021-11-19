2eb5bf0c649ad4e9f85b3a62e116030243f7fa4b
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-18 21:09:17 -0500

    created put route for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,8 +40,22 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const catData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!catData[0]) {
+      res.status(404).json({ message: "No Category with this id!" });
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
